6136ea5ec9621a484019fb89095b9b543eba522b
magoumi <agoumihunter@gmail.com>
2024-02-20 17:55:33 +0100

    refactor for better readability

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,26 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn get_validated_guess(attempt: u8) -> u32 {
-    // read and validate guess
+const MIN_NUMBER: u32 = 1;
+const MAX_NUMBER: u32 = 99;
+const MAX_ATTEMPTS: u8 = 6;
+
+fn get_validated_guess(attempts_left: u8) -> u32 {
     loop {
-        println!("Please input your guess, you have {attempt} attempts left");
+        println!("Please input your guess, you have {attempts_left} attempts left");
 
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Filed to read line");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("please enter valid number");
+                println!("Please enter a valid number");
                 continue;
             }
         };
 
-        if guess < 1 || guess > 99 {
-            println!("number must be >= 1 and <= 99");
+        if guess < MIN_NUMBER || guess > MAX_NUMBER {
+            println!("Number must be >= {MIN_NUMBER} and <= {MAX_NUMBER}");
             continue;
         }
 
@@ -29,27 +32,26 @@ fn get_validated_guess(attempt: u8) -> u32 {
 fn main() {
     println!("Guess the number");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    let mut attempt: u8 = 6;
+    let secret_number = rand::thread_rng().gen_range(MIN_NUMBER..=MAX_NUMBER);
+    let mut attempts_left: u8 = MAX_ATTEMPTS;
 
     loop {
-        if attempt == 0 {
-            println!("you lost, correct number is {secret_number}");
+        if attempts_left == 0 {
+            println!("You lost, the correct number is {secret_number}");
             break;
         }
 
-       let guess: u32 = get_validated_guess(attempt);
+        let guess: u32 = get_validated_guess(attempts_left);
 
-        println!("You guessed {guess}");
-        attempt -= 1;
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You won in {} attempts!", 6 - attempt);
+                println!("You won with {attempts_left} attempts remaining!");
                 break;
             }
         }
+
+        attempts_left -= 1;
     }
 }
